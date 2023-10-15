8ca8bbb168bf04426afcd3dc912ce0d4f0029b9b
3nd3r_h3o <theenderman3333@gmail.com>
2023-10-15 15:27:08 +0200

    default floor continued

--- src/main/java/h3o/ender/blocks/RegisterBlocks.java
+++ src/main/java/h3o/ender/blocks/RegisterBlocks.java
@@ -2,19 +2,22 @@ package h3o.ender.blocks;
 
 import h3o.ender.DwMod;
 import h3o.ender.blocks.tardis.DefaultFloor;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class RegisterBlocks {
-    public static final Block TARDIS_DEFAULT_FLOOR = new DefaultFloor(FabricBlockSettings.create().strength(4.0f));
+    public static final Block TARDIS_DEFAULT_FLOOR = new DefaultFloor(FabricBlockSettings.create().strength(-1.0f));
 
 
     public static void register() {
         DwMod.LOGGER.info("Registering Blocks");
         Registry.register(Registries.BLOCK, new Identifier(DwMod.MODID, "tardis.default.floor"), TARDIS_DEFAULT_FLOOR);
+        Registry.register(Registries.ITEM, new Identifier(DwMod.MODID, "tardis.default.floor"), new BlockItem(TARDIS_DEFAULT_FLOOR, new FabricItemSettings()));
     }
     
 }
