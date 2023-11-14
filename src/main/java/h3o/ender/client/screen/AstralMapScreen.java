ca6dc73a0678cafbdb1ab50451d9cf945b669f57
3nd3r_h3o <theenderman3333@gmail.com>
2023-11-14 15:29:56 +0100

    tilt added

--- src/main/java/h3o/ender/client/screen/AstralMapScreen.java
+++ src/main/java/h3o/ender/client/screen/AstralMapScreen.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 import h3o.ender.DwMod;
+import h3o.ender.components.Circuit;
+import h3o.ender.components.Circuit.NAME;
 import h3o.ender.screenHandler.AstralMapScreenHandler;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
@@ -11,15 +13,20 @@ import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.render.VertexFormat.DrawMode;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ColorHelper;
 
 public class AstralMapScreen extends HandledScreen<AstralMapScreenHandler> {
-    private final Identifier texture = new Identifier(DwMod.MODID, "textures/gui/frequency_detector_screen.png");
+    private final Identifier texture = new Identifier(DwMod.MODID, "textures/gui/astralmap.png");
 
     private static final List<Integer> planets = new ArrayList<>();
+    private double zoom = 0.9;
+    private double tilt = 45;
 
     public AstralMapScreen(AstralMapScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -27,8 +34,6 @@ public class AstralMapScreen extends HandledScreen<AstralMapScreenHandler> {
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
-        context.drawTexture(this.texture, this.x, this.y, 0, 0, this.backgroundWidth,
-        this.backgroundHeight);
     }
 
     @Override
@@ -38,17 +43,32 @@ public class AstralMapScreen extends HandledScreen<AstralMapScreenHandler> {
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
+        renderOrbitsAndGUI(context);
+        //TODO figure out why it don't work
+        renderPlanets(context);
+        
+    }
+
+    private void renderPlanets(DrawContext context) {
+        MatrixStack stack = context.getMatrices();
+        stack.push();
+        stack.translate((width/2) + 8, (height/2)+8, (width/2) + 8);
+        BakedModel model = client.getItemRenderer().getModel(Circuit.getItemForName(Circuit.nameToStr(NAME.DEFAULT_ROTOR)), null, null, 0);
+        this.client.getItemRenderer().renderItem(Circuit.getItemForName(Circuit.nameToStr(NAME.DEFAULT_ROTOR)), ModelTransformationMode.NONE, false, stack, context.getVertexConsumers(), 0, 0, model);
+        context.draw();
+        stack.pop();
+    }
+
+    private void renderOrbitsAndGUI(DrawContext context) {
+        context.getMatrices().push();
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        bufferBuilder.begin(DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
         for (int s = 1; s <= planets.size(); s++) {
-            context.getMatrices().push();
-            BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-            bufferBuilder.begin(DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
             int segments = 100;
-            double zoom = 0.9;
-            //deg
-            double tilt = 45;
-            int radius = ((int) Math.round((s * (width / 2)/ planets.size()) * zoom));
+            
+            // deg
+            int radius = ((int) Math.round((s * (width / 2) / planets.size()) * zoom));
             for (int i = 0; i < segments; i++) {
-                // gen the two points of the line
                 double sig1 = (double) i / segments * 2.0 * Math.PI;
                 double sig2 = (double) (i + 1) / segments * 2.0 * Math.PI;
                 double tet = (double) Math.toRadians(tilt % 360);
@@ -61,9 +81,8 @@ public class AstralMapScreen extends HandledScreen<AstralMapScreenHandler> {
                 double y2 = (height / 2) - Math.cos(sig2) * Math.sin(tet) * radius;
                 double z2 = Math.cos(sig2) * Math.cos(tet) * radius + radius;
 
-                //color
+                // color
                 int color = ColorHelper.Argb.getArgb(1, 245, 66, 66);
-
                 bufferBuilder
                         .vertex(context.getMatrices().peek().getPositionMatrix(), (float) x1, (float) y1, (float) z1)
                         .color(color).next();
@@ -71,11 +90,30 @@ public class AstralMapScreen extends HandledScreen<AstralMapScreenHandler> {
                         .vertex(context.getMatrices().peek().getPositionMatrix(), (float) x2, (float) y2, (float) z2)
                         .color(color).next();
             }
-            Tessellator.getInstance().draw();
-            context.getMatrices().pop();
-        }
 
+        }
+        Tessellator.getInstance().draw();
+        context.drawTexture(this.texture, 0, 0, 175, 1, 14, 14);
+        context.drawTexture(this.texture, 0, 14, 175, 15, 14, 14);
+        context.getMatrices().pop();
     }
+
+    
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (this.client.options.forwardKey.matchesKey(keyCode, scanCode)) {
+            tilt = (tilt+1) % 360;
+            return true;
+        }
+        if (this.client.options.backKey.matchesKey(keyCode, scanCode)) {
+            tilt = (tilt-1) % 360;
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+
 
     static {
         planets.add(Integer.valueOf(1));
